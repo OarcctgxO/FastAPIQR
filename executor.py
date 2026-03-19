7508ca79b4c38c5630e7c5dc2468902a226e764f
Aleksandr Tok <alexandrtok24@gmail.com>
2026-03-19 16:48:42 +0300

    Убрана ненужная переменная new. Переработан запуск сервера.

--- executor.py
+++ executor.py
@@ -8,18 +8,16 @@ def get_executor():
     try:
         python_version = (version_info.major, version_info.minor)
         gil = True
-        new = False
         interpreter = False
 
         if python_version >= (3, 13):
             gil = not bool(get_config_var("Py_GIL_DISABLED"))
-            new = True
         if python_version >= (3, 14):
             interpreter = True
 
         if gil and interpreter:
             cpu_exec = concurrent.futures.InterpreterPoolExecutor(cpu_count())
-        elif new and not gil:
+        elif not gil:
             cpu_exec = concurrent.futures.ThreadPoolExecutor(cpu_count())
         else:
             cpu_exec = concurrent.futures.ProcessPoolExecutor(cpu_count())
