6066f308725b50d90935a352fc61f916a7e0aa20
Aleksandr Tok <alexandrtok24@gmail.com>
2026-03-26 11:36:39 +0300

    Совместимость с PyInstaller

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI, Path, Response
-import uvicorn
+from uvicorn import run as uvicorn_run
 
-import asyncio
+from asyncio import get_running_loop
 from concurrent.futures import ProcessPoolExecutor
+from multiprocessing import freeze_support
 from typing import Annotated
 from threading import Thread
 
@@ -18,16 +19,17 @@ cpu_exec = ProcessPoolExecutor(workers)
 
 @app.get("/{str_to_code}")
 async def get_qr(str_to_code: Annotated[str, Path(min_length=1, max_length=1000)]):
-    loop = asyncio.get_running_loop()
+    loop = get_running_loop()
     fut = loop.run_in_executor(cpu_exec, make_qr_code, str_to_code)
     img_bytes = await fut
     return Response(content=img_bytes, media_type="image/png")
 
 
 if __name__ == '__main__':
+    freeze_support()
     if not is_there_running_server():
         udp_server = Thread(target=UdpDiscoverer().sync_work, daemon=True) # type: ignore
-        fastapi_server = Thread(target=uvicorn.run, args=("main:app", ), kwargs={'host': host, 'port': port}, daemon=True)
+        fastapi_server = Thread(target=uvicorn_run, args=(app, ), kwargs={'host': host, 'port': port, 'use_colors': False}, daemon=True)
         
         udp_server.start()
         fastapi_server.start()
